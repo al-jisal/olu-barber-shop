1e995bde137093940df68f2e3d3575c6ae235b76
al_jisal <aljisal.frimpong@gmail.com>
2026-09-23 19:34:25 -0400

    implemented the nav bar

---
+++ app/(backend)/constant.ts
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Book',
+    href: '#book',
+  },
+  {
+    id: 2,
+    name: 'Services',
+    href: '#services',
+  },
+  {
+    id: 3,
+    name: 'Sample',
+    href: '#sample',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
